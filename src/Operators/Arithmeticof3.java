05988e9a7294c2385bf137bfb18459b03ace444d
tmvargason <59941055+tmvargason@users.noreply.github.com>
2021-01-10 13:29:15 -0500

    Update Arithmeticof3.java

--- src/Operators/Arithmeticof3.java
+++ src/Operators/Arithmeticof3.java
@@ -5,9 +5,9 @@ public class Arithmeticof3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int a=10;
-		int b=20;
-		int c=100;
+		int a=6;
+		int b=16;
+		int c=26;
 		System.out.println("Sum of a and b and c is :"+ (a+b+c));
 
 	}
